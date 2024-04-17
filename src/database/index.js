481d4f1d2bd65d442ac3a8fcae1cb0d09d12cd94
thaynavazzoler <thayna.vazzoler@aluno.senai.br>
2024-04-17 15:20:39 -0300

    "New Commit"

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,5 @@
 import pgp from "pg-promise";
 import { config } from "dotenv";
-
 import path, { join } from "path";
 import { fileURLToPath } from "url";
 
@@ -12,22 +11,33 @@ const host = process.env.DB_HOST;
 const port = process.env.DB_PORT;
 const database = process.env.DB_NAME;
 
-const dbURL = `postgres://${user}:${password}@${host}:${port}/${database}`;
+const dbURL =
+  "postgres://" +
+  user +
+  ":" +
+  password +
+  "@" +
+  host +
+  ":" +
+  port +
+  "/" +
+  database;
 
-const pg = pgp()(dbURL);
+const db = pgp()(dbURL);
 
-export function connect () {
-    pg.query("SELECT 1+1 AS result").then((result) => {
-        console.log(result);
-    });
+export function createConnection() {
+  db.query("SELECT 1 + 1 AS result").then((result) => {
+    console.log(result);
+  });
 }
 
+// Captura o caminho do arquivo atual
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const filePath = join(__dirname, "create-tables.sql");
 const query = new pgp.QueryFile(filePath);
 
-pg.query(query);
+db.query(query);
 
-export default pg;
+export default db;
